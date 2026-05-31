195632a92e1a6f9d053d668d9ff84f0a23879db0
edgarmensah35-max <edgarmensah35@gmail.com>
2026-05-31 14:50:47 -0800

    Add initials printing functionality
    Implemented a program to print stylized initials 'EKM'.

--- initials.py
+++ initials.py
@@ -1,7 +1,35 @@
+  initials.py
+# Description: Print out my initials.
+# Assignment Number: 1
+#
+# Name: Edgar Kwaku Mensah
+# STUDENT ID: <2425402260>
+# Email: <2425402260@live.gctu.edu.gh>
+# Grader: Augustus Buckman
+#
+# On my honor, Edgar Kwaku Mensah, this programming assignment is my own work
+# and I have not provided this code to any other student.
+
 
 def main():
-  pass #code goes here
+    # Print stylized initials
 
+    print()
+    print("...EKM")
+    print()
+
+    print("...EEEEEEEEEEEE...KK........KK...MMMM....MMMM")
+    print("...EE.......... ...KK......KK....MM.MM..MM.MM")
+    print("...EE.......... ...KK....KK......MM..MMMM..MM")
+    print("...EEEEEEEEEE....KKKKKK...........MM...MM...MM")
+    print("...EE.......... ...KK....KK......MM........MM")
+    print("...EE.......... ...KK......KK....MM........MM")
+    print("...EE.......... ...KK.......KK...MM........MM")
+    print("...EE.......... ...KK........KK..MM........MM")
+    print("...EE.......... ...KK.........KK.MM........MM")
+    print("...EEEEEEEEEEEE...KK..........KK.MM........MM****")
+
+    print()
 
 
 main()
